9904ab641bd4e26f6e4400ed0ee75586f01d6853
Bennet <bexoda@hotmail.com>
2024-05-22 05:16:39 +0000

    Update to the migration Added select 2 to report form

--- AsseyLabMgt/Data/Migrations/20240522043351_allownullforlebresults.Designer.cs
+++ AsseyLabMgt/Data/Migrations/20240522043351_allownullforlebresults.Designer.cs
@@ -4,6 +4,7 @@ using AsseyLabMgt.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AsseyLabMgt.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240522043351_allownullforlebresults")]
+    partial class allownullforlebresults
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AsseyLabMgt/Data/Migrations/20240522043351_allownullforlebresults.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AsseyLabMgt.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class allownullforlebresults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Time",
+                table: "LabResults",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Time",
+                table: "LabResults",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "time",
+                oldNullable: true);
+        }
+    }
+}
